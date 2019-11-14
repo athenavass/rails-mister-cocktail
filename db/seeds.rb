3fe5c10982deeed9710b39f27ee7b471de43678c
Athena Vassilopoulos <athena.vassilo@gmail.com>
2019-11-14 14:12:53 -0500

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning up"
+
+Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
+
+puts "Seeding!"
+
+Ingredient.create(name: 'lemon')
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "ginger")
+Ingredient.create(name: "turmeric")
+i = 0
+
+names = ["Paul", "Pierre", "Jacques", "Anna", "Marie"]
+
+5.times do
+  cocktail = Cocktail.create(name: names[i])
+
+  dose = Dose.new(description: "5ml")
+  dose.cocktail = cocktail
+  dose.ingredient = Ingredient.all[i]
+  dose.save
+  i += 1
+end
+
+puts "Done!"
