521987cfd57eece4f8e161c7a4493bf8db3b8f79
Athena Vassilopoulos <athena.vassilo@gmail.com>
2019-11-15 16:59:25 -0500

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  get 'doses/new'
-  get 'doses/create'
-  get 'doses/delete'
-  get 'cocktails/index'
-  get 'cocktails/show'
-  get 'cocktails/new'
-  get 'cocktails/create'
   resources :cocktails, only: [:index, :new, :create, :show] do
     resources :doses, only: [:new, :create]
   end
  resources :doses, only: [:destroy]
+ root "cocktails#index"
 end
